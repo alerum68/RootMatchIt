6bc81af41c4607c34c29be0b2023e3ef6d11dd06
alerum68 <jdcole99@gmail.com>
2024-07-04 23:45:59 -0700

    Version 2. Working file for new format.  Allows for new providers or changes to existing providers to be added easily.  Does not import into RM at this point, only gathers and process the data to be handled properly by RootsMagic. FTDNA, MH not added in yet, just placeholders.  All tables needed for Ancestry are gathered, but not mapped to RootsMagic yet. Fixed some shadowing errors with the variables, and cleaned the variables up a little bit, and prepared ftdna and mh for tables.

--- WIPs/import_from_ancestry.py
+++ WIPs/import_from_ancestry.py
@@ -28,10 +28,10 @@ def setup_logging():
         root_logger.addHandler(file_handler)
 
         # File handler for info and debug
-        file_handler = logging.FileHandler('DG2RM2.log')
-        file_handler.setLevel(logging.DEBUG)  # Set to DEBUG level for all messages
-        file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
-        root_logger.addHandler(file_handler)
+        # file_handler = logging.FileHandler('DG2RM2.log')
+        # file_handler.setLevel(logging.DEBUG)  # Set to DEBUG level for all messages
+        # file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+        # root_logger.addHandler(file_handler)
 
         # Console handler for info and debug
         console_handler = logging.StreamHandler()
@@ -77,8 +77,8 @@ def dna_kit_fact_type(cursor_rm):
         logging.error(f"Error inserting or updating 'DNA Kit' in FactTypeTable: {e}")
 
 
-def gather_data(conn_dg, sample_size=None):
-    data = {}
+def gather_data(conn_dg, size=None):
+    local_data = {}
     cursor_dg = conn_dg.cursor()
 
     try:
@@ -87,28 +87,25 @@ def gather_data(conn_dg, sample_size=None):
         }
 
         for provider, table_list in tables.items():
-            data[provider] = {}
+            local_data[provider] = {}
             for table in table_list:
                 query = f"SELECT * FROM {table}"
-                if sample_size:
-                    query += f" LIMIT {sample_size}"
+                if size:
+                    query += f" LIMIT {size}"
                 cursor_dg.execute(query)
                 rows = cursor_dg.fetchall()
                 headers = [description[0] for description in cursor_dg.description]
-                data[provider][table] = [dict(zip(headers, row)) for row in rows]
-
-        print("Data dictionary contents:")
-        print(data)
+                local_data[provider][table] = [dict(zip(headers, row)) for row in rows]
 
     except sqlite3.Error as e:
-        print(f"Error gathering data: {e}")
+        print(f"Error gathering ancestry_data: {e}")
     finally:
         cursor_dg.close()
 
-    return data
+    return local_data
 
 
-'''def process_user_kits(conn_rm, data):
+'''def process_user_kits(conn_rm, ancestry_data):
     kits = []
     idx = 1
 
@@ -122,11 +119,11 @@ def gather_data(conn_dg, sample_size=None):
             idx += 1
 
     # Display profiles
-    display_and_collect_kits("Ancestry_Profiles", data['Profiles'].get('Ancestry_Profiles', []))
+    display_and_collect_kits("Ancestry_Profiles", ancestry_data['Profiles'].get('Ancestry_Profiles', []))
 
     # Process DNA kits by provider
-    ftdna_kits = [kit for kit in data['Profiles'].get('DNA_Kits', []) if kit['company'] == 'FTDNA']
-    myheritage_kits = [kit for kit in data['Profiles'].get('DNA_Kits', []) if kit['company'] == 'MyHeritage']
+    ftdna_kits = [kit for kit in ancestry_data['Profiles'].get('DNA_Kits', []) if kit['company'] == 'FTDNA']
+    myheritage_kits = [kit for kit in ancestry_data['Profiles'].get('DNA_Kits', []) if kit['company'] == 'MyHeritage']
 
     display_and_collect_kits("FTDNA", ftdna_kits, 'FTDNA')
     display_and_collect_kits("MyHeritage", myheritage_kits, 'MyHeritage')
@@ -161,34 +158,33 @@ def gather_data(conn_dg, sample_size=None):
             dna_kits.append((company_id, guid, given_name, surname))
         import_rm(conn_rm, dna_kits)
     except sqlite3.Error as e:
-        logging.error(f"Error processing and inserting data: {e}")
+        logging.error(f"Error processing and inserting ancestry_data: {e}")
     finally:
         if cursor_rm:
             cursor_rm.close()'''
 
 
-def process_ancestry(data):
+def process_ancestry(ancestry_data):
     standardized_data_icw = []
     standardized_data_match_trees = []
     standardized_data_match_groups = []
     standardized_data_profiles = []
 
-    def get_value(row, key, default=None):
+    def get_value(a_row, key, default=None):
         try:
-            return row[key]
+            return a_row[key]
         except KeyError:
-            logging.warning(f"Missing key '{key}' in row. Using default value '{default}'.")
             return default
 
     try:
         # Data inspection logging
-        logging.info(f"Data inspection - Ancestry_ICW: {data.get('Ancestry_ICW', [])[:5]}")
-        logging.info(f"Data inspection - Ancestry_matchTrees: {data.get('Ancestry_matchTrees', [])[:5]}")
-        logging.info(f"Data inspection - Ancestry_matchGroups: {data.get('Ancestry_matchGroups', [])[:5]}")
-        logging.info(f"Data inspection - Ancestry_Profiles: {data.get('Ancestry_Profiles', [])[:5]}")
+        logging.info(f"Data inspection - Ancestry_ICW: {ancestry_data.get('Ancestry_ICW', [])[:5]}")
+        logging.info(f"Data inspection - Ancestry_matchTrees: {ancestry_data.get('Ancestry_matchTrees', [])[:5]}")
+        logging.info(f"Data inspection - Ancestry_matchGroups: {ancestry_data.get('Ancestry_matchGroups', [])[:5]}")
+        logging.info(f"Data inspection - Ancestry_Profiles: {ancestry_data.get('Ancestry_Profiles', [])[:5]}")
 
-        # Process Ancestry_ICW data
-        for row in data.get('Ancestry_ICW', []):
+        # Process Ancestry_ICW ancestry_data
+        for row in ancestry_data.get('Ancestry_ICW', []):
             standardized_row = {
                 'ID1': get_value(row, 'matchid'),
                 'ID2': get_value(row, 'icwid'),
@@ -205,10 +201,10 @@ def process_ancestry(data):
             }
             standardized_data_icw.append(standardized_row)
 
-        logging.info(f"Ancestry ICW data standardized successfully: {standardized_data_icw}")
+        logging.info(f"Ancestry ICW ancestry_data standardized successfully: {standardized_data_icw}")
 
-        # Process Ancestry_matchTrees data
-        for row in data.get('Ancestry_matchTrees', []):
+        # Process Ancestry_matchTrees ancestry_data
+        for row in ancestry_data.get('Ancestry_matchTrees', []):
             standardized_row = {
                 'matchGuid': get_value(row, 'matchGuid'),
                 'Surname': get_value(row, 'surname'),
@@ -229,10 +225,10 @@ def process_ancestry(data):
             }
             standardized_data_match_trees.append(standardized_row)
 
-        logging.info(f"Ancestry matchTrees data standardized successfully: {standardized_data_match_trees}")
+        logging.info(f"Ancestry matchTrees ancestry_data standardized successfully: {standardized_data_match_trees}")
 
-        # Process Ancestry_matchGroups data
-        for row in data.get('Ancestry_matchGroups', []):
+        # Process Ancestry_matchGroups ancestry_data
+        for row in ancestry_data.get('Ancestry_matchGroups', []):
             match_display_name = get_value(row, 'matchTestDisplayName', '')
             if ' ' in match_display_name:
                 given_name, surname = match_display_name.rsplit(' ', 1)
@@ -269,60 +265,112 @@ def process_ancestry(data):
             }
             standardized_data_match_groups.append(standardized_row)
 
-        logging.info(f"Ancestry matchGroups data standardized successfully: {standardized_data_match_groups}")
+        logging.info(f"Ancestry matchGroups ancestry_data standardized successfully: {standardized_data_match_groups}")
 
-        # Process Ancestry_Profiles data
-        for row in data.get('Ancestry_Profiles', []):
+        # Process Ancestry_Profiles ancestry_data
+        for row in ancestry_data.get('Ancestry_Profiles', []):
             standardized_row = {
                 'guid': get_value(row, 'guid'),
                 'name': get_value(row, 'name')
             }
             standardized_data_profiles.append(standardized_row)
 
-        logging.info(f"Ancestry Profiles data standardized successfully: {standardized_data_profiles}")
+        logging.info(f"Ancestry Profiles ancestry_data standardized successfully: {standardized_data_profiles}")
 
     except KeyError as e:
-        logging.error(f"Error standardizing Ancestry data: Missing key {e}")
+        logging.error(f"Error standardizing Ancestry ancestry_data: Missing key {e}")
 
     return (standardized_data_icw, standardized_data_match_trees,
             standardized_data_match_groups, standardized_data_profiles)
 
 
-def process_ftdna(data):
-    # Process and standardize FTDNA data
+def process_ftdna(ftdna_data):
+    # Process and standardize FTDNA ancestry_data
     standardized_data = []
+
+    def get_value(f_row, key, default=None):
+        try:
+            return f_row[key]
+        except KeyError:
+            return default
     try:
-        for row in data['FTDNA_Matches2']:
+        for row in ftdna_data['FTDNA_Matches2']:
             standardized_row = {
-                'Given': row['name'].split(' ')[0] if ' ' in row['name'] else row['name'],
-                'Surname': row['name'].split(' ')[1] if ' ' in row['name'] else '',
-                'MatchGuid': row['match_guid'],  # FTDNA_Matches2.match_guid
-                'SharedCM': row['shared_cm'],  # FTDNA_Matches2.shared_cm
-                'SharedSegments': row['shared_segments']  # FTDNA_Matches2.shared_segments
+                'Id': get_value(row, 'Id'),
+                'DNAProvider': "2",
+                'Surname': get_value(row, 'surname'),
+                'Given': get_value(row, 'given_name'),
+                'ID1': get_value(row, 'testGuid'),
+                'ID2': get_value(row, 'matchGuid'),
+                'Group': get_value(row, 'groupName'),
+                'SharedCM': get_value(row, 'sharedCentimorgans'),
+                'SharedSegments': get_value(row, 'sharedSegment'),
+                'LastLoggedInDate': get_value(row, 'lastLoggedInDate'),
+                'Starred': get_value(row, 'starred'),
+                'Viewed': get_value(row, 'viewed'),
+                'MatchTreeIsPrivate': get_value(row, 'matchTreeIsPrivate'),
+                'Note': get_value(row, 'note'),
+                'UserPhoto': get_value(row, 'userPhoto'),
+                'MatchTreeId': get_value(row, 'matchTreeId'),
+                'TreeId': get_value(row, 'treeId'),
+                'CreatedDate': get_value(row, 'created_date'),
+                'LoginUsername': get_value(row, 'loginUsername'),
+                'Sync': get_value(row, 'sync'),
+                'Paternal': get_value(row, 'paternal'),
+                'Maternal': get_value(row, 'maternal'),
+                'Sex': get_value(row, 'subjectGender'),
+                'MeiosisValue': get_value(row, 'meiosisValue'),
+                'ParentCluster': get_value(row, 'parentCluster')
             }
             standardized_data.append(standardized_row)
-        logging.info("FTDNA data standardized successfully.")
+        logging.info("FTDNA ancestry_data standardized successfully.")
     except KeyError as e:
-        logging.error(f"Error standardizing FTDNA data: Missing key {e}")
+        logging.error(f"Error standardizing FTDNA ancestry_data: Missing key {e}")
     return standardized_data
 
 
-def process_mh(data):
-    # Process and standardize MyHeritage data
+def process_mh(mh_data):
+    # Process and standardize MyHeritage ancestry_data
     standardized_data = []
+
+    def get_value(h_row, key, default=None):
+        try:
+            return h_row[key]
+        except KeyError:
+            return default
     try:
-        for row in data['MH_Match']:
+        for row in mh_data['MH_Match']:
             standardized_row = {
-                'Given': row['name'].split(' ')[0] if ' ' in row['name'] else row['name'],
-                'Surname': row['name'].split(' ')[1] if ' ' in row['name'] else '',
-                'MatchGuid': row['match_guid'],  # MH_Match.match_guid
-                'SharedCM': row['shared_cm'],  # MH_Match.shared_cm
-                'SharedSegments': row['shared_segments']  # MH_Match.shared_segments
+                'Id': get_value(row, 'Id'),
+                'DNAProvider': "2",
+                'Surname': get_value(row, 'surname'),
+                'Given': get_value(row, 'Given Name'),
+                'ID1': get_value(row, 'testGuid'),
+                'ID2': get_value(row, 'matchGuid'),
+                'Group': get_value(row, 'groupName'),
+                'SharedCM': get_value(row, 'sharedCentimorgans'),
+                'SharedSegments': get_value(row, 'sharedSegment'),
+                'LastLoggedInDate': get_value(row, 'lastLoggedInDate'),
+                'Starred': get_value(row, 'starred'),
+                'Viewed': get_value(row, 'viewed'),
+                'MatchTreeIsPrivate': get_value(row, 'matchTreeIsPrivate'),
+                'Note': get_value(row, 'note'),
+                'UserPhoto': get_value(row, 'userPhoto'),
+                'MatchTreeId': get_value(row, 'matchTreeId'),
+                'TreeId': get_value(row, 'treeId'),
+                'CreatedDate': get_value(row, 'created_date'),
+                'LoginUsername': get_value(row, 'loginUsername'),
+                'Sync': get_value(row, 'sync'),
+                'Paternal': get_value(row, 'paternal'),
+                'Maternal': get_value(row, 'maternal'),
+                'Sex': get_value(row, 'subjectGender'),
+                'MeiosisValue': get_value(row, 'meiosisValue'),
+                'ParentCluster': get_value(row, 'parentCluster')
             }
             standardized_data.append(standardized_row)
-        logging.info("MyHeritage data standardized successfully.")
+        logging.info("MyHeritage ancestry_data standardized successfully.")
     except KeyError as e:
-        logging.error(f"Error standardizing MyHeritage data: Missing key {e}")
+        logging.error(f"Error standardizing MyHeritage ancestry_data: Missing key {e}")
     return standardized_data
 
 
@@ -357,7 +405,7 @@ def import_rm(conn_rm, dna_kits):
             else:
                 logging.error(f"Failed to Create or Update DNA Kit for '{given_name} {surname}' for '{guid}'.")
     except sqlite3.Error as e:
-        logging.error(f"Error importing data into RootsMagic database: {e}")
+        logging.error(f"Error importing ancestry_data into RootsMagic database: {e}")
     finally:
         if cursor_rm:
             cursor_rm.close()
@@ -365,16 +413,16 @@ def import_rm(conn_rm, dna_kits):
 
 
 """def import_trees(conn_rm, tree_data):
-    # Import family tree data into RootsMagic
+    # Import family tree ancestry_data into RootsMagic
     cursor_rm = None  # Initialize cursor_rm to None
     try:
         cursor_rm = conn_rm.cursor()
         for row in tree_data:
-            # Implement the logic to import tree data
+            # Implement the logic to import tree ancestry_data
             ...
-        logging.info("Family tree data imported successfully.")
+        logging.info("Family tree ancestry_data imported successfully.")
     except sqlite3.Error as e:
-        logging.error(f"Error importing family tree data: {e}")
+        logging.error(f"Error importing family tree ancestry_data: {e}")
     finally:
         if cursor_rm:
             cursor_rm.close()
@@ -392,21 +440,21 @@ if __name__ == "__main__":
             sample_size = 50  # Set sample size here if needed
             data = gather_data(dnagedcom_conn, sample_size)
 
-            # Process user data
-            # process_user_kits(rootsmagic_conn, data)
+            # Process user ancestry_data
+            # process_user_kits(rootsmagic_conn, ancestry_data)
 
-            # Process data for each provider
+            # Process ancestry_data for each provider
             standardized_ancestry_data = process_ancestry(data['Ancestry'])
-            # standardized_ftdna_data = process_ftdna(data['FTDNA'])
-            # standardized_mh_data = process_mh(data['MyHeritage'])
+            # standardized_ftdna_data = process_ftdna(ancestry_data['FTDNA'])
+            # standardized_mh_data = process_mh(ancestry_data['MyHeritage'])
 
-            # Import standardized data into RootsMagic
+            # Import standardized ancestry_data into RootsMagic
             # import_rm(rootsmagic_conn, standardized_ancestry_data)
             # import_rm(rootsmagic_conn, standardized_ftdna_data)
             # import_rm(rootsmagic_conn, standardized_mh_data)
 
             # Import family trees if needed
-            # import_trees(rootsmagic_conn, data['Ancestry'])  # Adjust as necessary
+            # import_trees(rootsmagic_conn, ancestry_data['Ancestry'])  # Adjust as necessary
 
             dnagedcom_conn.close()
         else:
